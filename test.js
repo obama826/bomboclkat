511948b38f7b9246ca22805164af4a47b0305b89
obama826 <150904289+obama826@users.noreply.github.com>
2025-09-16 11:25:52 +0700

    Update test.js

--- test.js
+++ test.js
@@ -1,3 +1,14 @@
-function clickFunction() {
-    document.getElementById("main-heading").innerHTML = "now im javascript woa"
-}
+
+
+const button = document.getElementById('button');
+const message = document.getElementById('main-heading');
+
+let clickCounter = 0;
+const baseText = "The button has been clicked";
+
+button.addEventListener('click', function() {
+clickCounter++;
+message.textContent = `${baseText} ${clickCounter} times.`;
+button.style.backgroundColor = '#90EE90';
+
+});
